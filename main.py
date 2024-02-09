15d919aa47bf8c5bc272862a3f60ded598633c80
Russell Rathbun <crash6215@gmail.com>
2024-02-08 22:30:06 -0600

    Moved more stuff into function calls for readability

--- main.py
+++ main.py
@@ -25,6 +25,22 @@ green = (17, 122, 13)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #title setup
 def title():
     font = pygame.font.Font('freesansbold.ttf', 32)
@@ -56,29 +72,38 @@ def events():
         if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
             # Toggle fullscreen mode
             pygame.display.toggle_fullscreen()
-
+        # if user types QUIT then the screen will close 
+        if event.type == pygame.QUIT: 
+            pygame.quit() 
+            sys.exit() 
     return
-
+def background():
+    #Wiping after logo
+    pygame.display.set_caption('Player Selection')
+    screen.fill("black")
+    title()
+    # Setup player selection environment
+    pygame.draw.rect(screen, red, pygame.Rect(100, 50, X/2, Y-150))
+    pygame.draw.rect(screen, green, pygame.Rect((X/2), 50, X/2-100, Y-150))
 
 
 def game():
     running = True
     splashScreen()
     screen = pygame.display.set_mode((desktop.current_w, desktop.current_h))
+    background()
     while running:
         X = int(screen.get_width())
         Y = int(screen.get_height())
-        #Wiping after logo
-        pygame.display.set_caption('Player Selection')
-        screen.fill("black")
-        title()
-        # Setup player selection environment
-        pygame.draw.rect(screen, red, pygame.Rect(100, 50, X/2, Y-150))
-        pygame.draw.rect(screen, green, pygame.Rect((X/2), 50, X/2-100, Y-150))
         pygame.display.flip()
         clock.tick(60)  # limits FPS to 60
         events()
+        print('fps is: ' + str(clock.get_fps()))
 # Run Game
 game()
+
+
+
+
 
 
