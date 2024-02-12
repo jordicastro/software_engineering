7e0c233e1330aef1b64f8b7a21315c6140cac587
BatSmacker84 <ooohhhwells@gmail.com>
2024-02-12 08:55:06 -0600

    no more weird venv

--- lib/python3.10/site-packages/supafunc/utils.py
+++
@@ -1,9 +0,0 @@
-from httpx import AsyncClient as AsyncClient  # noqa: F401
-from httpx import Client as BaseClient
-
-__version__ = "0.3.3"
-
-
-class SyncClient(BaseClient):
-    def aclose(self) -> None:
-        self.close()
