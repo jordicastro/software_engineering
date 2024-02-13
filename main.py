c6211d15a318dcaad8c3ab7ac76902c613182c9e
Russell Rathbun <crash6215@gmail.com>
2024-02-13 15:37:01 -0600

    Added textboxes and stuff

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
 from splash import splashScreen
+from inputs import InputBox
 import pygame
 import sys
+
 # pygame setup
 pygame.init()
-
 desktop = pygame.display.Info()
 screen = pygame.display.set_mode((desktop.current_w-10, desktop.current_h-50))
 clock = pygame.time.Clock()
@@ -24,12 +24,20 @@ def title():
     textRect = title.get_rect()
     textRect.center = (X // 2, textRect.bottom)
     return screen.blit(title, textRect)
+
+def textBox(input, bg, textColor, x, y):
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    text = font.render(input, True, (textColor), (bg))
+    textRect = text.get_rect()
+    textRect.center = (x, y)
+    return screen.blit(text, textRect)
+
 #Event function
 def events():
     # Check for events
     for event in pygame.event.get():
         # Check for the fullscreen toggle event
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
             # Toggle fullscreen mode
             pygame.display.toggle_fullscreen()
             background()
@@ -43,11 +51,24 @@ def background():
     pygame.display.set_caption('Player Selection')
     screen.fill("black")
     title()
+    redX = 100
+    redY = 75
+    redW = X/2
+    redH = Y-150
+    greenX = X/2
+    greenY = 75
+    greenW = X/2-100
+    greenH = Y-150
     # Setup player selection environment
-    pygame.draw.rect(screen, red, pygame.Rect(100, 50, X/2, Y-150))
-    pygame.draw.rect(screen, green, pygame.Rect((X/2), 50, X/2-100, Y-150))
-
-
+    pygame.draw.rect(screen, red, pygame.Rect(redX, redY, redW, redH))
+    pygame.draw.rect(screen, green, pygame.Rect(greenX, greenY, greenW, greenH))
+    textBox("ID", red, "white", (-100+redX+redW/4), redY+30)
+    textBox("ID", green, "white", (greenX+greenW/4), greenY+30)
+    textBox("Name", red, "white", -100+redX+3*(redW/4), redY+30)
+    textBox("Name", green, "white", greenX+3*(greenW/4), greenY+30)
+    
+    
+    
 def game():
     running = True
     splashScreen()
@@ -57,14 +78,8 @@ def game():
         X = int(screen.get_width())
         Y = int(screen.get_height())
         pygame.display.flip()
-        clock.tick(600)  # limits FPS to 60
+        clock.tick(60)  # limits FPS to 60
         events()
-        print('fps is: ' + str(clock.get_fps()))
+
 # Run Game
 game()
-
-
-
-
-
-
