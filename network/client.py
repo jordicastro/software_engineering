8a92c6d2d9fb0aa447ac6ff102c6f2f38588343d
Jordi Castro <jordi.castro003@gmail.com>
2024-02-15 13:14:14 -0600

    udp communication implemented

--- network/client.py
+++ network/client.py
@@ -10,8 +10,8 @@ class Client:
     def __init__(self, SERVER='127.0.0.1', PORT=7500) -> None:
         self.ADDR = (SERVER, PORT)
         self.FORMAT = 'utf-8'
+        print('setting up udp socket for client1')
         self.client = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        # self.client.connect(self.ADDR)
 
     def send(self, msg):
         message = msg.encode(self.FORMAT)
@@ -19,13 +19,17 @@ class Client:
 
     # transmit equipment codes after each player addition
     def send_id(self, player_id): # sends id of newly created player & id of player hit
+        print('in send_id method.')
+        print(f'player_id: {player_id}')
         message = player_id.encode(self.FORMAT)
+        print(f'encoded player_id message: {message}')
         self.client.sendto(message, self.ADDR)
 
 # client = Client()
 # client.send('test')
-        
+print("creating player1.")        
 player1 = Client()
+print("calling send_id method.")
 player1.send_id('0123456')
 
 # player2 = Client()
--- network/server.py
+++ network/server.py
@@ -18,10 +18,11 @@ def handle_client(conn, addr): # conn is socket obj, addr is info about connecti
     connected = True
     while connected:
         data, addr = server.recvfrom(1024) #receive data from client
+        print(f'[RECEIVED DATA] received data from client: {data}')
         msg = data.decode(FORMAT) # decode data
         print(f'[{addr}] {msg}')
 
-        conn.sendto(f'msg {msg} received'.encode(FORMAT), addr) # send response back to client
+        server.sendto(f'msg {msg} received'.encode(FORMAT), addr) # send response back to client
 def start():
     print(f'[LISTENING] server is listening on {server}')
 
@@ -29,7 +30,7 @@ def start():
         data, addr = server.recvfrom(1024)
         thread = threading.Thread(target=handle_client, args=(data, addr)) # send info to handle_client
         thread.start()
-        print(f'[ACTIVE CONECTIONS] {threading.activeCount() -1}')
+        print(f'[ACTIVE CONECTIONS] {threading.active_count() -1}')
 
 print('[START] server is starting...')
 start()
