1cd3a3ba59133405b3d09ebc8fe72c006b75daba
Russell Rathbun <crash6215@gmail.com>
2024-02-19 12:10:48 -0600

    Finished player selection screen for now Added button, text input, and text line classes to gui.py Redid the sizing of the player selection screen stuff, will definitely revisit later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from splash import splashScreen
-from inputs import InputBox
+from gui import InputLine, Button
 import pygame
 import sys
 
@@ -58,52 +58,54 @@ def background():
     pygame.display.set_caption('Player Selection')
     screen.fill("black")
     title()
-    redX = 100
+    redX = X/2 - 100 - 400 - 100  # middle of screen, 100 left of box, 400 for width of boxes, 100 right of boxes
     redY = 75
-    redW = X/2
-    redH = Y-150
+    redW = 100+400+100            #100 left of box, 400 for width of boxes, 100 right of boxes
+    redH = 650
     greenX = X/2
     greenY = 75
-    greenW = X/2-100
-    greenH = Y-150
+    greenW = 100 + 400 + 100
+    greenH = 650
     # Setup player selection environment
     pygame.draw.rect(screen, red, pygame.Rect(redX, redY, redW, redH))
     pygame.draw.rect(screen, green, pygame.Rect(greenX, greenY, greenW, greenH))
-    textBox("ID", red, "white", (-100+redX+redW/4)+20, redY+30)
-    textBox("ID", green, "white", (greenX+greenW/4)+20, greenY+30)
-    textBox("Name", red, "white", -100+redX+3*(redW/4), redY+30)
-    textBox("Name", green, "white", greenX+3*(greenW/4), greenY+30)
+    textBox("ID", red, "white", redX+100+100, redY+30)
+    textBox("ID", green, "white", greenX+100+100, greenY+30)
+    textBox("Name", red, "white", redX+redW-100-100, redY+30)
+    textBox("Name", green, "white", greenX+greenW-100-100, greenY+30)
     
 def inputBoxLoad():
     inputBoxes = []
     numberBoxes = 15
     startY = 140
     boxHeight = 32
+    boxWidth = 200
     endHeight = numberBoxes * boxHeight + startY
-    redBoxX = ((X/2-100)/4+100)/2
-    greenBoxX = X/2+(X/2-100)/4 -80
+    redBoxX = X/2-100-400
+    greenBoxX = X/2+100
     for i in range(startY,endHeight,boxHeight): # range(starting y, ending y, increment y)
-        temp = InputBox(redBoxX, i, 140, boxHeight) # X, Y, W, H
+        temp = InputLine(redBoxX, i, boxWidth, boxHeight) # X, Y, W, H
         inputBoxes.append(temp)
     for i in range(startY,endHeight,boxHeight): # range(starting y, ending y, increment y)
-        temp = InputBox(greenBoxX, i, 140, boxHeight) # X, Y, W, H
+        temp = InputLine(greenBoxX, i, boxWidth, boxHeight) # X, Y, W, H
         inputBoxes.append(temp)
     return inputBoxes
-    
+def onStart():
+    print('Start pressed')
 def game():
     running = True
     splashScreen()
     screen = pygame.display.set_mode((desktop.current_w, desktop.current_h))
+    X = int(screen.get_width())
+    Y = int(screen.get_height())
     background()
     input_boxes = inputBoxLoad()
+    startButton = Button(X/2-40, Y/2+200, 70, 32, 'Start', onStart)
     while running:
-        X = int(screen.get_width())
-        Y = int(screen.get_height())
-        for box in input_boxes:
-            box.update()
         background()
         for box in input_boxes:
             box.draw(screen)
+        startButton.process(screen)
         pygame.display.flip()
         clock.tick(60)  # limits FPS to 60
         input_boxes = events(input_boxes)
